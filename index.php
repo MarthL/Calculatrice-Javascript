2e17a34db4fe2c46defb93b45770afdf4be7d2fa
Enzo Franceschini <enzo.franceschinidev@gmail.com>
2021-02-25 14:37:33 +0100

    modif style.css and index.php

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
-<body>
 
+<body>
+    <div class="calculette">
+        <div class="ecran">
+            <p id="ecranUtilisateur">
+
+                hello
+            </p>
+        </div>
+        <div class="btn" id="1"></div>
+        <div class="btn" id="2"></div>
+        <div class="btn" id="3"></div>
+        <div class="btn" id="4"></div>
+        <div class="btn" id="5"></div>
+        <div class="btn" id="6"></div>
+        <div class="btn" id="7"></div>
+        <div class="btn" id="8"></div>
+        <div class="btn" id="9"></div>
+        <div class="btn" id="0"></div>
+        <div class="btn" id="more"></div>
+        <div class="btn" id="less"></div>
+        <div class="btn" id="divise"></div>
+        <div class="btn" id="soustraction"></div>
+        <div class="btn" id="delete"></div>
+        <div class="btn" id="result"></div>
+    </div>
+
+
+    <script src="./main.js"></script>
 </body>
+
 </html>
